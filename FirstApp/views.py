82287b70b6e2820dfa204e6d9766bd83979bb553
joeljeshurun <joeljeshurun@gmail.com>
2018-03-08 13:15:23 -0500

    project files added

---
+++ FirstApp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    a=5+5
+    my_dict={'insert_me':'inserted from view','second_key':'Again from view'}
+    return render(request,'FirstApp/index.html',my_dict)
+
+def home(request):
+    return HttpResponse("WELCOME TO HOME")
