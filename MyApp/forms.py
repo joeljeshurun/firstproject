78b1014a9c6bffa3a05c64cdecc61cc8b04f12ed
joeljeshurun <joeljeshurun@gmail.com>
2018-03-14 13:56:09 -0400

    Model form

--- MyApp/forms.py
+++ MyApp/forms.py
@@ -1,6 +1,35 @@
 from django import forms
+from django.forms.widgets import HiddenInput
+from django.core import validators
+from MyApp.models import User
 
-class FormName(forms.Form):
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-    email_id = forms.EmailField()
+class FormName(forms.ModelForm):
+    first_name = forms.CharField(validators=[validators.MinLengthValidator(3)])
+    class Meta:
+        model=User
+        fields='__all__'
+        # exclude=['first_name']
+        # fields=['first_name']
+
+# class FormName(forms.Form):
+#     first_name = forms.CharField()
+#     last_name = forms.CharField()
+#     email_id = forms.EmailField()
+    # verify_email = forms.EmailField()
+    # botcatcher = forms.CharField(required=False,widget=HiddenInput,validators=[validators.MaxLengthValidator(0)])
+
+    # def clean_botcatcher(self):
+    #     botcatch = self.cleaned_data['botcatcher']
+    #     if len(botcatch)>0:
+    #         raise forms.ValidationError('Bot detected')
+    #
+    # def clean_first_name(self):
+    #     first = self.cleaned_data['first_name']
+    #     if first[0].lower() !='z':
+    #         raise forms.ValidationError('First Name should start with Z')
+    #     return first
+
+    # def clean(self):
+    #     get_all = super().clean()
+    #     if get_all['email_id']!=get_all['verify_email']:
+    #         raise forms.ValidationError("Emails do not match!")
--- MyApp/views.py
+++ MyApp/views.py
@@ -16,10 +16,17 @@ def user(request):
 def form_detail_view(request):
     form = FormName()
     if request.method == 'POST':
+        print(form.is_valid())
         form = FormName(request.POST)
         if form.is_valid():
-            print(form.cleaned_data['first_name'])
-            print(form.cleaned_data['last_name'])
-            print(form.cleaned_data['email_id'])
+            form.save()
+
+            # first = form.cleaned_data['first_name']
+            # last = form.cleaned_data['last_name']
+            # email = form.cleaned_data['email_id']
+
+            #User.objects.get_or_create(first_name=first, last_name=last, email_id=email)
+            # user = User(first_name=first, last_name=last, email_id=email)
+            # user.save()
     return render(request, 'MyApp/form_details.html',{'form':form})
 
