4be923658f4bf77807fc5d78cec380d64d89081b
joeljeshurun <joeljeshurun@gmail.com>
2018-03-26 14:01:54 -0400

    Latest

--- FirstApp/models.py
+++ FirstApp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-
+# create model
 class User(models.Model):
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
